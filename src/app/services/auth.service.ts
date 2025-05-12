bdb2568d2855460fa98947f76934ba8246d5f579
rodoet9 <102057385+rodoet9@users.noreply.github.com>
2025-05-12 11:02:10 +0300

    Update auth.service.ts

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -23,7 +23,7 @@ export class AuthService {
   signup(email: string, password: string) {
     return this.http
       .post<AuthResponseData>(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDpS5i1zRQ6tT1XL-rMoE1tkmQnhrpdXAI',
+        'https://identitytoolkit.googleapis.com/v1/API_KEY',
         {
           email: email,
           password: password,
@@ -46,7 +46,7 @@ export class AuthService {
   login(email: string, password: string) {
     return this.http
       .post<AuthResponseData>(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDpS5i1zRQ6tT1XL-rMoE1tkmQnhrpdXAI',
+        'https://identitytoolkit.googleapis.com/v1/API_KEY',
         {
           email: email,
           password: password,
